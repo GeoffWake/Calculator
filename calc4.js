c8204ecde7c1f28cf249c991d142f9f039094a39
Geoffrey Wakeford <you@example.com>
2022-05-06 10:34:56 +1000

    Finished

--- calc4.js
+++ calc4.js
@@ -1,20 +1,10 @@
-////Things to do
-///Limit display screen to 5 character
-////add decimal point
-////On/off button to do something
-////Wrap
+///Starting Values
 
-const buttonsIds = ["b0", "b1", "b2", "b3", "b4", "b5", "b6", "b7", "b8", "b9"];
+let storage = [];
 
-const  display= document.querySelector('#screen')
+let storage2 = [];
 
-
-
-///Operator buttons
-
-let storage = []
-
-let storage2 = []
+let dec= '';
 
 let currentNumber1= " ";
 
@@ -22,55 +12,80 @@ let operator= "";
 
 let displayValue ="0";
 
-const operators = document.querySelectorAll(".operator");
- let previousNumber = ""
+//Button Array
+const buttonsIds = ["b0", "b1", "b2", "b3", "b4", "b5", "b6", "b7", "b8", "b9"];
 
+///Variables/Selectors
 
+///Screen
+const  display= document.querySelector('#screen')
 
+///Decimal
+const decimal =document.getElementById("bdec");
+ 
+///Multiply 
+const multiply =document.getElementById("btimes");
+
+////Divide
+const divide =document.getElementById("bdiv");
+
+///Minus
+const minus =document.getElementById("bmin");
+
+///Plus
+const plus =document.getElementById("bplus");
+
+///Equal 
+const equalButton =document.getElementById("bequal");
+
+///On/Clear
+const onclear =document.getElementById('on');
+
+///Clear Screen
+const clearScreen= document.getElementById('clear');
+
+///Add decimal point to display and currentNumber1
+
+decimal.addEventListener('click', () => inputDecimal("."));
+
+const inputDecimal = (i) => {
+   
+  currentNumber1 += i;
+  
+}
+
+///Loop for button selector
 
 for (let i = 0; i < buttonsIds.length; i++) {
     const button = document.getElementById(buttonsIds[i]);
     button.addEventListener('click', () => displayNumber(i))
     };
 
-
+///Display Number on Screen
 
 const displayNumber =(i) => {
     currentNumber1 += i;
-    display.textContent=currentNumber1
-    //console.log(currentNumber1)
-    //console.log(typeof currentNumber1)
-    
-
+    display.innerHTML=currentNumber1
+    console.log(currentNumber1)
 }
 
+////Event Listeners for buttons
 
+///Multiply
 
-////Operators
-
-const multiply =document.getElementById("btimes");
-    
 multiply.addEventListener('click', () => inputOperator("*"));
 
-////Divide
-const divide =document.getElementById("bdiv");
-    
+///Divide
 divide.addEventListener('click', () => inputOperator("/"));
 
 ///Minus
-
-const minus =document.getElementById("bmin");
-    
 minus.addEventListener('click', () => inputOperator("-"));
 
-
-////Plus
-const plus =document.getElementById("bplus");
-    
+///Plus
 plus.addEventListener('click', () => inputOperator("+"));
 
 
-////Displaying on screen
+////Display and add Operator
 
 const inputOperator = (n) => {
   if (operator.length >=  1) return; 
@@ -80,83 +95,66 @@ const inputOperator = (n) => {
   console.log(operator)
 }
 
-//Organise an alert for when operator button is pressed and current data is stored in an array
+//When operator button is pressed the first number is stored in an array called "storage"
 
 document.querySelectorAll('.operator').forEach(item => {
   item.addEventListener('click', event => {
     storage.push(currentNumber1)
-    currentNumber1=""
-   
+    currentNumber1="" 
     console.log(storage)
-    //console.log(display.textContent)
   })
 })
 
-///When Equals button is pressed "second Number" is also stored in an array
-const equalButton =document.getElementById("bequal");
+///When Equals button is pressed "second Number" is also stored in second array called "storage2".
+
+///Switch statement is then done and the appropriate calculation is completed
     
 equalButton.addEventListener('click', event => {
-  
+  ///Store "second number" in an array
   storage2.push(currentNumber1)
- // console.log(storage)
-    console.log(storage2) 
+    console.log(storage2)
+
     switch(operator){
       case '-':
-     // parseInt.storage
-     // parseInt.storage2 
-     // console.log(storage)
-      result=parseInt(storage)-(parseInt(storage2))
+      result=storage-storage2
       display.textContent = result 
-    // console.log((parseInt(storage))-(parseInt(storage2)))
         break;
       case '+':
-        result=parseInt(storage)+(parseInt(storage2))
+        ///Parsefloat addition to prevent concatenation.
+        result=parseFloat(storage)+parseFloat(storage2)
         console.log(result)
         display.textContent = result 
          break; 
-         case '+':
-          result=parseInt(storage)+(parseInt(storage2))
-          console.log(result)
-          display.textContent = result 
-           break; 
         case '*':
-          result=parseInt(storage)*(parseInt(storage2))
+          result=storage*storage2
           console.log(result)
           display.textContent = result 
             break;
         case '/':
-          result=parseInt(storage)/(parseInt(storage2))
+          result=storage/storage2
           console.log(result)
           display.textContent = result 
             break;  
     }
+    
 })
 
 
-///Switch statement for when equals button is pressed. Turn array into numbers, perform calculation and then result and you a FUCKING DONE
 
 
-///Clear Button
-//console.log(storage)
+///Clear action
 
-const clearScreen= document.getElementById('clear');
 clearScreen.addEventListener("click",() => clear() )
-
 
 const clear = () => {
  currentNumber1 = "";
  operator      = "";
- display.textContent=displayValue;;
+ display.textContent=displayValue;
  storage = [];
  storage2= [];
+ dec= ""
 }
-///Next step
-///When equals button is pressed store 2nd number in array
-////then converry to number
-////if statement regarding operator function
 
-///clear button should clear both arrays as well
+///On button/clear
 
-///Need to do max amount of digits
-
-///Calculation--YEAH BUDDYY!!!!!
+onclear.addEventListener("click",() => clear() )
